d67a1bff4060a7b4a0cf90894d8988c34b644578
Matt Tartaglia <mattbrs51@gmail.com>
2020-06-01 12:47:49 -0400

    created script to access my java webservice, successfully communicating with my API

--- src/App.js
+++ src/App.js
@@ -1,41 +1,28 @@
 import React, { Component } from 'react'
-// import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css';
-// import HeaderComponent from './components/HeaderComponent'
-import LoginComponent from './components/LoginComponent'
-// import ButtonComponent from './components/ButtonComponent'
-import LandingComponent from './components/LandingComponent'
-// import FunctionComponent from './components/examples/FunctionComponent'
-// import Counter from './components/counter/Counter'
+import './bootstrap.css';
+import HeaderComponent from './components/HeaderComponent'
+import WelcomeComponent from './components/WelcomeComponent';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
  
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LandingComponent></LandingComponent>
-        <LoginComponent></LoginComponent>
+        <Router>
+          <>
+          <HeaderComponent></HeaderComponent>
+          <Switch>
+            <Route path="/welcome/:name" component={WelcomeComponent}/>
+          </Switch>
+        </>
+        </Router>
       </div>
     )
   }
 }
 
-
-
-
-
-// class LearningComponents extends Component {
-//   render() {
-//     return (
-//       <div className="learningComponents">
-//        <HeaderComponent></HeaderComponent>
-//        <LoginComponent></LoginComponent>
-//        <ButtonComponent></ButtonComponent>
-//        <LandingComponent></LandingComponent>
-//        <FunctionComponent></FunctionComponent>   
-//       </div>
-//     )
-//   }
-// }
 
 export default App;
 
--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,11 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 
-export default class HeaderComponent extends Component {
+class HeaderComponent extends Component {
     render() {
         return (
-            <div className="headerComponent">
-            <h1>This is the header</h1>
-            </div>
+            <header>
+                <nav className="navbar navbar-expand-md navbar-dark bg-success">
+                    <div className="navbar-brand">MyApp</div>
+                    <ul className="navbar-nav">
+                        <li><Link className="nav-link" to="/">Home</Link></li>
+                        <li><Link className="nav-link" to="/">Content</Link></li>
+                    </ul>
+                    <ul className="navbar-nav navbar-collapse justify-content-end">
+                        <li><Link className="nav-link" to="/">Login</Link></li>
+                        <li><Link className="nav-link" to="/">Logout</Link></li>
+                    </ul>
+                </nav>
+            </header>
         )
     }
 }
+
+export default HeaderComponent
